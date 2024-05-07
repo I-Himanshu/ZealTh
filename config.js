0299d40e7b9984403682b03ba69dd4934a1bf0dd
Ashish Kumar <90233808+DeveloperAshish8@users.noreply.github.com>
2024-05-07 09:37:18 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
+  apiKey: "AIzaSyBdhmNhkomDFetCCU_YW-LrFLXTdFPcs3I",
+  authDomain: "zealth-4.firebaseapp.com",
+  projectId: "zealth-4",
+  storageBucket: "zealth-4.appspot.com",
+  messagingSenderId: "749463136880",
+  appId: "1:749463136880:web:773a831ba3577da9e422b5",
+  measurementId: "G-DF7JXH3410",
 };
 
 // Initialize Firebase
